f62418879845a1061584c458171c477d04e0e83b
WillMS28 <silva.miranda.william@gmail.com>
2021-06-22 12:30:06 -0300

    create buttonAdd component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,12 +4,15 @@ import { View } from "react-native";
 import { styles } from './styles'
 
 import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
 
 export function Home() {
   return (
     <View>
       <View style={styles.header}>
         <Profile />
+
+        <ButtonAdd />
       </View>
     </View>
   )
