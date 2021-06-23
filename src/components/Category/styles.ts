25f716b48343299d17f45a0bdf9d2b6893870618
WillMS28 <silva.miranda.william@gmail.com>
2021-06-23 10:19:32 -0300

    Gradient Adjustment in Category Component

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -1,4 +1,3 @@
-import { Inter_100Thin } from '@expo-google-fonts/inter'
 import { StyleSheet } from 'react-native'
 import { theme } from '../../global/theme'
 
@@ -15,36 +14,37 @@ export const styles = StyleSheet.create({
   content: {
     width: 100,
     height: 116,
-    backgroundColor: theme.colors.secondary40,
-    justifyContent: 'space-between',
     alignItems: 'center',
+    justifyContent: 'space-between',
     borderRadius: 8,
-    paddingVertical: 7
+    paddingVertical: 20
   },
 
   title: {
-    fontFamily: theme.fonts.title500,
+    fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
     fontSize: 15
   },
 
   check: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
     width: 12,
     height: 12,
     backgroundColor: theme.colors.secondary100,
-    alignSelf: 'flex-end',
-    marginRight: 7,
     borderColor: theme.colors.secondary50,
     borderWidth: 2,
     borderRadius: 3,
   },
 
   checked: {
+    position: 'absolute',
+    top: 7,
+    right: 7,
     width: 10,
     height: 10,
     backgroundColor: theme.colors.primary,
-    alignSelf: 'flex-end',
-    marginRight: 7,
     borderColor: theme.colors.secondary50,
     borderRadius: 3,
   }
