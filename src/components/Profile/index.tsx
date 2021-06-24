b71da7ae3782fcb61859c46c16de0df05a766b89
WillMS28 <silva.miranda.william@gmail.com>
2021-06-24 15:34:24 -0300

    using user info on profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 
 import { Avatar } from '../Avatar'
 
 import { styles } from './styles'
 
 export function Profile() {
+  const { user } = useAuth()
+
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage='https://github.com/WillMS28.png'/>      
+      <Avatar urlImage={user.avatar}/>      
 
       <View>
         <View style={styles.user}>
@@ -17,7 +20,7 @@ export function Profile() {
             Olá,
           </Text>
           <Text style={styles.username}>
-            William 
+            { user.firstName } 
           </Text>
         </View>
 
