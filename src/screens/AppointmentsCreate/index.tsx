5e2a0ff990106159a9cd33c02d8cbf0155606c87
WillMS28 <silva.miranda.william@gmail.com>
2021-06-25 12:35:50 -0300

    getting image from database for AppointmentCreate

--- src/screens/AppointmentsCreate/index.tsx
+++ src/screens/AppointmentsCreate/index.tsx
@@ -79,7 +79,7 @@ export function AppointmentsCreate() {
             <RectButton onPress={handleOpenGuilds}>
               <View style={styles.select}>
                 { 
-                  guild.icon ? <GuildIcon /> : <View style={styles.image}/>
+                  guild.icon ? <GuildIcon guildId={guild.id} iconId={guild.icon} /> : <View style={styles.image}/>
                 }
                 <View style={styles.selectBody}>
                   <Text style={styles.label}>
